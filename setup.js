e3381dbd5cd40ed31bf44f08cdddd78a17dc208e
NJF289 <122659161+NJF289@users.noreply.github.com>
2024-10-25 14:16:47 +0000

    teste

--- setup.js
+++ setup.js
@@ -1,28 +1,28 @@
-playerNamesDiv.innerHTML = '';
-
-if (numPlayers < 1 || numPlayers > 4) {
-        //alert("O número de jogadores deve ser entre 1 e 4.");
-       
-} else {
-for (let i = 1; i <= numPlayers; i++) {
-const input = document.createElement('input');
-input.type = 'text';
-input.name = `player${i}`;
-input.placeholder = `Nome do Jogador ${i}`;
-input.required = true;
-playerNamesDiv.appendChild(input);
-}
-} ;
-
-document.getElementById('setup-form').addEventListener('submit', function(event) {
-event.preventDefault();
-const formData = new FormData(this);
-const players = [];
-formData.forEach((value, key) => {
-if (key.startsWith('player')) {
-players.push(value);
-}
-});
-localStorage.setItem('players', JSON.stringify(players));
-window.location.href = 'game/game.html';
-});
+document.getElementById('num-players').addEventListener('change', function() {
+        const numPlayers = this.value;
+        const playerNamesDiv = document.getElementById('player-names');
+        playerNamesDiv.innerHTML = '';
+    
+        for (let i = 1; i <= numPlayers; i++) {
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.name = `player${i}`;
+            input.placeholder = `Nome do Jogador ${i}`;
+            input.required = true;
+            playerNamesDiv.appendChild(input);
+        }
+    });
+    
+    document.getElementById('setup-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const formData = new FormData(this);
+        const players = [];
+        formData.forEach((value, key) => {
+            if (key.startsWith('player')) {
+                players.push(value);
+            }
+        });
+        localStorage.setItem('players', JSON.stringify(players));
+        window.location.href = 'game.html';
+    });
+    
